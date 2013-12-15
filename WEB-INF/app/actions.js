997f28d10af19b1e23160541005b5d22d3e2f850
Philipp Naderer <philipp@Philipps-MacBook-2.local>
2013-12-15 22:28:34 +0100

    Better styles / more features.

--- WEB-INF/app/actions.js
+++ WEB-INF/app/actions.js
@@ -1,5 +1,9 @@
 importPackage(com.google.appengine.api.users);
+importPackage(com.google.appengine.api.images);
 importPackage(com.google.appengine.api.datastore);
+importPackage(com.google.appengine.api.blobstore);
+
+var strings = require("ringo/utils/strings");
 
 var {Application} = require("stick");
 var app = exports.app = new Application();
@@ -44,10 +48,11 @@ var getCredentials = function(req) {
 };
 
 var mapProperties = function(entity) {
+   var parentKey = entity.getParent();
    var map = entity.getProperties();
    var obj = {
       id: entity.getKey().getId(),
-      parentId: entity.getParent().getId()
+      parentId: (parentKey !== null ? parentKey.getId() : null)
    };
 
    map.keySet().toArray().forEach(function(key) {
@@ -136,6 +141,7 @@ app.get("/bookshelf/:id", function(req, id) {
    // Retrieve books of this bookshell
    var books = datastore.prepare((new Query("Book")).setAncestor(bookshelf.getKey()).addSort("titleLowerCase", Query.SortDirection.ASCENDING)).asList(FetchOptions.Builder.withDefaults());
    var booksArray = [];
+
    for (var i = 0; i < books.size(); i++) {
       booksArray.push(mapProperties(books.get(i)));
    }
@@ -173,7 +179,7 @@ app.post("/bookshelf/:id", function(req, id) {
    var credentials = getCredentials(req);
    if (credentials.isLoggedIn && credentials.currentUser.equals(bookshelf.getProperty("creator"))) {
       if (req.postParams.title && req.postParams.title.length >= 1 &&
-         ((req.postParams.year == null && req.postParams.year == "") || filterInt(req.postParams.year, 10) !== NaN)) {
+         (req.postParams.year == "" || filterInt(req.postParams.year, 10) !== NaN)) {
          var datastore = DatastoreServiceFactory.getDatastoreService();
          
          var book = new Entity("Book", bookshelf.getKey());
@@ -183,7 +189,13 @@ app.post("/bookshelf/:id", function(req, id) {
          book.setProperty("creator", credentials.currentUser);
          book.setProperty("author", req.postParams.author);
          book.setProperty("publisher", req.postParams.publisher);
-         book.setProperty("year", java.lang.Integer.parseInt(req.postParams.year));
+
+         if (req.postParams.year !== "") {
+            book.setProperty("year", java.lang.Integer.parseInt(req.postParams.year));
+         } else {
+            book.setProperty("year", null);
+         }
+
          book.setProperty("edition", req.postParams.edition);
          book.setProperty("isbn13", req.postParams.isbn13);
          book.setProperty("isbn10", req.postParams.isbn10);
@@ -217,11 +229,130 @@ app.get("/bookshelf/:shelfId/:bookId", function(req, shelfId, bookId) {
       return response.error().html("<h1>Internal error</h1>");
    }
 
+   // Check if request url is consistent
+   if (!bookshelf.getKey().equals(book.getParent())) {
+      return response.notFound().html("<h1>Book not found</h1>");
+   }
+
    // Check if the user is the owner of a bookshelf
    var credentials = getCredentials(req);
-   if (credentials.isLoggedIn && credentials.currentUser.equals(bookshelf.getProperty("creator"))) {      
-      return response.text(mapProperties(book).toSource());
+   if (credentials.isLoggedIn && credentials.currentUser.equals(bookshelf.getProperty("creator"))) {
+      return response.html(env.getTemplate("bookAdmin.html").render({
+         title: book.getProperty("title"),
+         book: mapProperties(book),
+         bookshelf: mapProperties(bookshelf),
+         username: credentials.principal.getName(),
+         logoutURI: credentials.logoutURI
+      }));
+   }
+
+   return response.html(env.getTemplate("book.html").render({
+      title: book.getProperty("title"),
+      book: mapProperties(book),
+      bookshelf: mapProperties(bookshelf)
+   }));
+});
+
+app.get("/bookshelf/:shelfId/:bookId/upload", function(req, shelfId, bookId) {
+   var datastore = DatastoreServiceFactory.getDatastoreService();
+   var bookshelf, bookshelfKey; 
+   try {
+      bookshelfKey = KeyFactory.createKey("Bookshelf", java.lang.Long.parseLong(shelfId));
+      bookshelf = datastore.get(bookshelfKey);
+   } catch(e if e.javaException instanceof EntityNotFoundException) {
+      return response.notFound().html("<h1>Bookshelf not found</h1>");
+   } catch (e) {
+      return response.error().html("<h1>Internal error</h1>");
+   }
+
+   var book; 
+   try {
+      book = datastore.get(bookshelfKey.getChild("Book", java.lang.Long.parseLong(bookId)));
+   } catch(e if e.javaException instanceof EntityNotFoundException) {
+      return response.notFound().html("<h1>Book not found</h1>");
+   } catch (e) {
+      return response.error().html("<h1>Internal error</h1>");
+   }
+
+   // Check if the user is the owner of a bookshelf
+   var credentials = getCredentials(req);
+   if (credentials.isLoggedIn && credentials.currentUser.equals(bookshelf.getProperty("creator")) &&
+         bookshelf.getKey().equals(book.getParent())) {
+      
+      // BlobstoreService to create upload url
+      var blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+
+      return response.html(env.getTemplate("uploadCover.html").render({
+         uploadUrl: blobstoreService.createUploadUrl("/bookshelf/" + shelfId + "/" + bookId + "/upload"),
+         title: book.getProperty("title"),
+         book: mapProperties(book),
+         bookshelf: mapProperties(bookshelf),
+         username: credentials.principal.getName(),
+         logoutURI: credentials.logoutURI
+      }));
    }
 
    return response.redirect("/bookshelf/" + id);
+});
+
+app.post("/bookshelf/:shelfId/:bookId/upload", function(req, shelfId, bookId) {
+   var datastore = DatastoreServiceFactory.getDatastoreService();
+   var bookshelf, bookshelfKey;
+   var invalidCall = false;
+
+   try {
+      bookshelfKey = KeyFactory.createKey("Bookshelf", java.lang.Long.parseLong(shelfId));
+      bookshelf = datastore.get(bookshelfKey);
+   } catch (e) {
+      invalidCall = true;
+   }
+
+   var book; 
+   try {
+      book = datastore.get(bookshelfKey.getChild("Book", java.lang.Long.parseLong(bookId)));
+   } catch (e) {
+      invalidCall = true;
+   }
+
+   // BlobstoreService to create upload url
+   var blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+   var blobs = blobstoreService.getUploads(req.env.servletRequest);
+
+   // Check if the user is the owner of a bookshelf
+   var credentials = getCredentials(req);
+   if (!invalidCall && credentials.isLoggedIn && credentials.currentUser.equals(bookshelf.getProperty("creator")) &&
+         bookshelf.getKey().equals(book.getParent())) {
+      
+      // Get the uploaded cover blob
+      var blobKeys = blobs.get("coverFile");
+
+      // Drop blobs if upload was invalid
+      if(blobs.size() !== 1 || blobKeys == null || blobKeys.size() !== 1) {
+
+      } else {
+         var blobInfos = blobstoreService.getBlobInfos(req.env.servletRequest).get("coverFile");
+
+         // Check uploaded mime types
+         if (strings.startsWith(blobInfos.get(0).getContentType(), "image/")) {
+            var imagesService = ImagesServiceFactory.getImagesService();
+
+            // Store references to the blob
+            book.setProperty("coverKey", blobKeys.get(0));
+            book.setProperty("coverUrl", imagesService.getServingUrl(ServingUrlOptions.Builder.withBlobKey(blobKeys.get(0))));
+            book.setProperty("coverUrlSecure", imagesService.getServingUrl(ServingUrlOptions.Builder.withBlobKey(blobKeys.get(0)).secureUrl(true)));
+            datastore.put(book);
+
+            return response.redirect("/bookshelf/" + shelfId + "/" + bookId);
+         }
+      }
+   }
+
+   // Delete Blobs
+   blobs.values().toArray().forEach(function(keyList) {
+      keyList.toArray().forEach(function(key) {
+         blobstoreService.delete(key);
+      });
+   });
+
+   return response.redirect("/bookshelf/" + shelfId);
 });
